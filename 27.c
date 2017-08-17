5c6f277e3c39a7ce09a8a97a625640b4e1787ff9
DineshMurugavel <30723921+DineshMurugavel@users.noreply.github.com>
2017-08-17 15:53:10 +0530

    Update 27.c

--- 27.c
+++ 27.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    int n1, n2, minMultiple;
+    int n1, n2, minM;
     printf("Enter two positive integers: ");
     scanf("%d %d", &n1, &n2);
-    minMultiple = (n1>n2) ? n1 : n2;
+    minM = (n1>n2) ? n1 : n2;
     while(1)
     {
-        if( minMultiple%n1==0 && minMultiple%n2==0 )
+        if( minM%n1==0 && minM%n2==0 )
         {
-            printf("The LCM of %d and %d is %d.", n1, n2,minMultiple);
+            printf("The LCM of %d and %d is %d.", n1, n2,minM);
             break;
         }
-        ++minMultiple;
+        ++minM;
     }
     return 0;
 }
